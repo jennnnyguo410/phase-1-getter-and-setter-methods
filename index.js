7b4b0ca1b8f32af10f3564403d82e9003da052f3
Hao Guo <jennnnyguo@gmail.com>
2023-06-28 10:49:57 +1000

    commit tree 4fd4688822fa8ca408fdc65ddee17a327ee9d2da

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius
+    }
+
+    get diameter() {
+        return this.radius * 2
+    }
+    get circumference() {
+        return (this.radius * 2) * PI
+    }
+    get area() {
+        return (Math.pow(this.radius)) * PI
+    }
+}
